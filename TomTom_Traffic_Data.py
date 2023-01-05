b9b1d3cbe1a9fae89f59631aa2b08056402c2d51
Olamylo <olaoyecharles1@gmail.com>
2023-01-05 01:19:31 +0100

    Add scripts to upload data to Azure blobs

--- TomTom_Traffic_Data.py
+++ TomTom_Traffic_Data.py
@@ -6,6 +6,18 @@ import time
 import pyodbc
 from sqlalchemy import create_engine
 import os
+from azure.storage.blob import BlobServiceClient
+
+# Replace with your Azure Storage Account name and key
+account_name = os.environ.get("account_name")
+account_key = os.environ.get("account_key")
+
+# Create the connection string
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key}"
+# connection_string = 'DefaultEndpointsProtocol=https;AccountName=account_name;AccountKey= account_key;EndpointSuffix=core.windows.net'
+
+# Create the BlobServiceClient using the connection string
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 
 def get_date(df,bulkid,date_added,location):
@@ -14,11 +26,13 @@ def get_date(df,bulkid,date_added,location):
     df.insert(2, 'location', location)
     return df
 
+
 nigeria = os.environ.get("nigeria")
 warsaw = os.environ.get("warsaw")
 losangeles = os.environ.get("losangeles")
 
 locations = [losangeles,warsaw,nigeria]
+# locations = ["USA_los-angeles","POL_warsaw","NGA_lagos"]
 # locations = ["USA_los-angeles","POL_warsaw","NGA_lagos"]
 
 count = 0
@@ -42,14 +56,35 @@ for location in locations:
     df.UpdateTimeWeekAgo = pd.to_datetime(df.UpdateTimeWeekAgo, unit="ms")
     df.UpdateTime = pd.to_datetime(df.UpdateTime, unit="ms")
 
-
     bulkid = time.strftime('%Y%m%d%H%M')
     date_added = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
     df = get_date(df,bulkid,date_added,location)
     list_df = pd.concat([list_df, df])
 
-    new_count = count + 1
+    # new_count = count + 1
+    # df.to_csv('./test_tomtom.csv',index = False, encoding= "utf-8")
+# list_df.to_sql('STAGING_TOMTOM_TRAFFIC', con=engine, if_exists='replace', index=False)
 
 print(list_df)
+
+# Create a ContainerClient
+container_name = "pythontest"
+container = blob_service_client.get_container_client(container_name)
+
+# Create a BlobClient
+blob_name = "tomtom2_new.csv"
+blob = container.get_blob_client(blob_name)
+
+# If you want to be able to append data to the blob
+blob.create_append_blob()
+
+# If you want to create a new blob
+# blob.upload_blob(list_df.to_csv().encode())
+
+# Append data to blob
+blob.append_block(list_df.to_csv().encode())
+
+
+
 
